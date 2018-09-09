5621741d8b0da90a55033f588c126e23c196525f
israr2018 <israr.qr@gmail.com>
2018-09-09 10:24:53 +0500

    Access Controll Allow Origin is set

--- app.js
+++ app.js
@@ -34,7 +34,9 @@ app.use('/uploads',express.static('uploads'));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    //https://psmpapp.herokuapp.com
+   // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+   res.setHeader('Access-Control-Allow-Origin', 'https://psmpapp.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
