93243e89e2a388f908e8a3d16ce06bb0c525b697
israr2018 <israr.qr@gmail.com>
2018-11-07 05:46:14 +0100

    Last fix in app.js with package.json

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-//const config = require('./config/index');
+const config = require('./config/index');
 var express=require('express');
 var app=express();
-//var cors=require('cors');
-//app.use(cors({credentials: true, origin: true}));
-//const config = require('./config');
-//require('dotenv').config();
+var cors=require('cors');
+app.use(cors({credentials: true, origin: true}));
+const config = require('./config');
+require('dotenv').config();
 
 const logger = require('morgan');
 var bodyParser=require('body-parser');
 var mongoose=require('mongoose');
-//var db=mongoose.connect('mongodb://localhost:27017/bookApi');
-// var db=mongoose.connect(config.database.connection).then(()=>{
-// console.log("connect to the data base successfully.")
-// },(error)=>{
-// console.log(`Could not connect to database something goes wrong:${error}`);
-// });
+var db=mongoose.connect('mongodb://localhost:27017/bookApi');
+var db=mongoose.connect(config.database.connection).then(()=>{
+console.log("connect to the data base successfully.")
+},(error)=>{
+console.log(`Could not connect to database something goes wrong:${error}`);
+});
 
 //var db=mongoose.connect('mongodb://israr:Computer2018@ds249372.mlab.com:49372/sms_db');
 
@@ -36,42 +36,42 @@ else{
 // var  CarMakes=require('./models/CarMakes');
 // var  CarModels=require('./models/CarModels');
 // var  UserModel=require('./models/UserModel');
-// var school_model=require('./models/school_model');
-// var class_model=require('./models/class_model');
-// var student_reg_model=require('./models/student_reg_model');
-// var student_attendence_model=require('./models/student_attendence_model');
-// var fees_ledger_model=require('./models/fees_ledger');
+var school_model=require('./models/school_model');
+var class_model=require('./models/class_model');
+var student_reg_model=require('./models/student_reg_model');
+var student_attendence_model=require('./models/student_attendence_model');
+var fees_ledger_model=require('./models/fees_ledger');
 
-// app.use('/uploads',express.static('uploads'));
-// app.use(bodyParser.urlencoded({extended:true}));
-// app.use(bodyParser.json());
-/* var bookRouter=require('./Routes/bookRoutes')(Book);
+app.use('/uploads',express.static('uploads'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+// var bookRouter=require('./Routes/bookRoutes')(Book);
 var carAdsRouter=require('./Routes/CarAdsRoutes')(CarAds);
 var carMakesRouter=require('./Routes/CarMakesRoutes')(CarMakes);
 var carModelsRouter=require('./Routes/CarModelsRoutes')(CarModels);
 
-// var authRouter=require('./Routes/authRoute')(UserModel); */
-// var school_router=require('./Routes/school_route')(school_model);
-// var class_router=require('./Routes/class_route')(class_model);
-// var student_router=require('./Routes/student_route')(student_reg_model);
-// var attendence_router=require('./Routes/attendence_route')(student_attendence_model);
-// var fees_ledger_router=require('./Routes/test_route')(fees_ledger_model);
-/* app.use("/api/Books",bookRouter);
-app.use("/api/CarAds",carAdsRouter);
-app.use("/api/CarMakes",carMakesRouter);
-app.use("/api/CarModels",carModelsRouter);
-app.use("/api/authenticate",authRouter); */
+var authRouter=require('./Routes/authRoute')(UserModel); 
+var school_router=require('./Routes/school_route')(school_model);
+var class_router=require('./Routes/class_route')(class_model);
+var student_router=require('./Routes/student_route')(student_reg_model);
+var attendence_router=require('./Routes/attendence_route')(student_attendence_model);
+var fees_ledger_router=require('./Routes/test_route')(fees_ledger_model);
+//  app.use("/api/Books",bookRouter);
+// app.use("/api/CarAds",carAdsRouter);
+// app.use("/api/CarMakes",carMakesRouter);
+// app.use("/api/CarModels",carModelsRouter);
+// app.use("/api/authenticate",authRouter); 
 
-// app.use("/api/schools",school_router);
-// app.use("/api/classes",class_router);
-// app.use("/api/students",student_router);
-// app.use("/api/attendence",attendence_router);
-// app.use("/api/test",fees_ledger_router);
-var port=process.env.PORT;
+app.use("/api/schools",school_router);
+app.use("/api/classes",class_router);
+app.use("/api/students",student_router);
+app.use("/api/attendence",attendence_router);
+app.use("/api/test",fees_ledger_router);
+//var port=process.env.PORT;
 app.get('/',function(req,res) {
     res.send("Welcome PSMS  api services... ");
 });
-app.listen(port,function () {
-   // console.log("server is running on port: "+ config.port);
-   console.log("server is running");
+app.listen(config,function () {
+   console.log("server is running on port: "+ config.port);
+   
 });
