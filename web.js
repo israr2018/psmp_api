ea3fb0b73e4e5c2656597144e726b2a51b49a8f7
israr2018 <israr.qr@gmail.com>
2018-11-07 07:00:40 +0100

    105

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var port=process.env.PORT
 app.get('/',function(req,res){
  res.send("Hello Israr");
 });
-app.listen(config,function(){
+app.listen(config.port,function(){
    // console.log("Gulp 2 is Running on PORT"+port);
     console.log("config.."+config.port);
 });
